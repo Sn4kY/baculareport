cef0626dfe2caefceb1da38168ab8522e19a23e7
Pierre DOLIDON <sn4ky@sn4ky.net>
2018-10-02 16:16:18 +0200

    unlinking customers

--- details.php
+++ details.php
@@ -46,7 +46,7 @@ $result->execute(array($clientId));
 		else $Depassement = $JobBytes - $VolFactu;
 		printf("<td>%s</td>",FileSizeConvert($row['TotalMaxFull']));
 		printf("<td>%s</td>",$JobHBytes);
-		printf("<td>%s <a href=\"edit.php&clientId=%s\"><img src=\"res/edit.png\" alt=\"edit\" /></a></td>",$VolHFactu, $clientId);
+		printf("<td>%s <a href=\"edit.php?clientId=%s\"><img src=\"res/edit.png\" alt=\"edit\" /></a></td>",$VolHFactu, $clientId);
 		if ($Depassement > 0) {
 			$HDepassement=FileSizeConvert($Depassement);
 			printf("<td>%s <img src=res/fouet.gif /></td>",$HDepassement);
@@ -59,10 +59,24 @@ $result->execute(array($clientId));
 	$result->closeCursor();
 ?>
 </table>
+
+<?php
+//Unlinking client<=>customer
+if (isset($_GET['unlink']) && isset($_GET['serverId']) && isset($_GET['clientId'])) {
+	if ($_GET['unlink'] == true) {
+		$SQLunlink = $bdd->prepare('
+			DELETE FROM client_customer_assoc WHERE id_client=? AND customer_id=?
+		');
+
+		$SQLunlink->execute(array($_GET['serverId'],$_GET['clientId']));
+	}
+}
+?>
+
 <p></p>
 <table class="table table-striped table-bordered table-hover table-condensed">
 	<caption>Details du client <?php echo $clientName; ?></caption>
-	<tr class="warning"><th>Serveur</th><th>Filer</th><th>Plus gros Full</th><th>Total Full+Diff</th></tr>
+	<tr class="warning"><th>Serveur</th><th>Filer</th><th>Plus gros Full</th><th>Total Full+Diff</th><th>Action</th></tr>
 <?php
 $result = $bdd->prepare('
 SELECT grp.name, MAX(Job.JobBytes) AS BigFull, sum(Job.JobBytes) TotalFull, grp.id_client, sto_assoc.storage_name
@@ -81,13 +95,14 @@ while ($row = $result->fetch()) {
 	printf("<td>%s</td>",$row[4]);
 	printf("<td>%s</td>",FileSizeConvert($row[1]));
 	printf("<td>%s</td>",FileSizeConvert($row[2]));
+	printf("<td><a href=\"details.php?clientId=%s&serverId=%s&unlink=true\"><img src=\"res/unlink.png\" alt=\"unlink\" /></a></td>", $clientId , $row[3]);
 	echo "</tr>";
 }
 $result->closeCursor();
 echo "</table>";
 
 //Per server details
-if (isset($_GET['serverId'])) {
+if (isset($_GET['serverId']) && !isset($_GET['unlink'])) {
 ?>
 <table class="table table-striped table-bordered table-hover table-condensed">
 	<tr class="success"><th>JobId</th><th>Level</th><th>SchedTime</th><th>StartTime</th><th>EndTime</th><th>Duration</th><th>Nombre de fichier</th><th>Volume backup&eacute;</th><th>Job Status</th></tr>
